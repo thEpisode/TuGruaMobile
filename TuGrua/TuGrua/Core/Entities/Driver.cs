156cc27be527dd542ef5e4d66608b58d7cdcf7f3
Cami Rodriguez <camiepisode@outlook.com>
2016-02-06 21:07:38 -0500

    Added more cleaned code

--- TuGrua/TuGrua/Core/Entities/Driver.cs
+++ TuGrua/TuGrua/Core/Entities/Driver.cs
@@ -19,79 +19,22 @@ namespace TuGrua.Core.Entities
         
 		public static async Task<Driver> Create(Authentication auth, bool isTestUser = false)
         {
-			Console.WriteLine (auth);
+			//Console.WriteLine (auth);
 			return await CreateDriver(auth);
         }
 
 		private static async Task<Driver> CreateDriver(Authentication authUser)
 		{
-			string uriParams = "?id=" + System.Uri.EscapeDataString (authUser.DetailedUserId._id);
 
-			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create (new Uri (
-				TuGrua.Core.Backend.Constants.UriServer +
-				TuGrua.Core.Backend.Constants.UriApi +
-				TuGrua.Core.Backend.Constants.GetDetailedUser + 
-				uriParams));
-
-			request.ContentType = "application/json";
-			request.Method = "GET";
-            //request.Headers["x-access-token"] = authUser.Token;
-			request.Headers.Add("x-access-token", authUser.Token);
-            try 
-			{
-
-				using (WebResponse response = await request.GetResponseAsync ()) 
-				{
-					using (var stream = response.GetResponseStream ()) 
-					{
-						// Use this stream to build a JSON document object:
-						try 
-						{
-							JsonValue jsonDoc = await Task.Run (() => JsonObject.Load (stream));
-							if (jsonDoc != null) {
-                               
-                                JsonObject result = jsonDoc as JsonObject;
-								return new Driver () 
-								{
-									_id = (string)result["_id"],
-									PhoneNumber = (string)result["PhoneNumber"],
-									Name = (string)result["Name"],
-									LastName = (string)result["LastName"],
-									GeneralCalification = (int)result["GeneralCalification"]
-								};
-							}
-
-						} 
-						catch (Exception)
-						{
-							return null;
-						}
-
-					}
-				}
-			} 
-			catch (System.Net.WebException ex) 
-			{
-				//if (ex.Status == WebExceptionStatus.ProtocolError) {
-				//	var response = ex.Response as HttpWebResponse;
-				//	if (response != null) {
-				//		switch ((int)response.StatusCode) {
-				//		case 404:
-				//			//DisplayAlert ("Aviso", "La aplicación no está actualizada, ya que está accediendo a rutas inexistentes", "OK");
-				//			break;
-				//		default:
-				//			break;
-				//		}
-
-				//	}
-				//} 
-				//else if (ex.Status == WebExceptionStatus.ConnectFailure) 
-				//{
-				//	//DisplayAlert ("Aviso", "La aplicación no está disponible.", "OK");
-				//}
-			}
-			return null;
-		}
+            return new Driver()
+            {
+                _id = authUser.DetailedUserId._id,
+                PhoneNumber = authUser.DetailedUserId.PhoneNumber,
+                Name = authUser.DetailedUserId.Name,
+                LastName = authUser.DetailedUserId.LastName,
+                GeneralCalification = authUser.DetailedUserId.GeneralCalification
+            };
+        }
     }
 }
 
--- TuGrua/TuGrua/Core/Entities/Requester.cs
+++ TuGrua/TuGrua/Core/Entities/Requester.cs
@@ -24,72 +24,14 @@ namespace TuGrua.Core.Entities
 
 		private static async Task<Requester> CreateRequester(Authentication authUser)
 		{
-			string uriParams = "?id=" + System.Uri.EscapeDataString (authUser.DetailedUserId._id);
-
-			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create (new Uri (
-				TuGrua.Core.Backend.Constants.UriServer +
-				TuGrua.Core.Backend.Constants.UriApi +
-				TuGrua.Core.Backend.Constants.GetDetailedUser + 
-				uriParams));
-			
-			request.ContentType = "application/json";
-			request.Method = "GET";
-            request.Headers["x-access-token"] = authUser.Token;
-
-            try 
-			{
-
-				using (WebResponse response = await request.GetResponseAsync ()) 
-				{
-					using (var stream = response.GetResponseStream ()) 
-					{
-						// Use this stream to build a JSON document object:
-						try 
-						{
-							JsonValue jsonDoc = await Task.Run (() => JsonObject.Load (stream));
-							if (jsonDoc != null) {
-                                
-
-                                JsonObject result = jsonDoc as JsonObject;
-								return new Requester () 
-								{
-									PhoneNumber = (string)result["PhoneNumber"],
-									Name = (string)result["Name"],
-									LastName = (string)result["LastName"],
-									GeneralCalification = (int)result["GeneralCalification"]
-								};
-							}
-
-						} 
-						catch (Exception ex)
-						{
-							return null;
-						}
-
-					}
-				}
-			} 
-			catch (System.Net.WebException ex) 
-			{
-				//if (ex.Status == WebExceptionStatus.ProtocolError) {
-				//	var response = ex.Response as HttpWebResponse;
-				//	if (response != null) {
-				//		switch ((int)response.StatusCode) {
-				//		case 404:
-				//			//DisplayAlert ("Aviso", "La aplicación no está actualizada, ya que está accediendo a rutas inexistentes", "OK");
-				//			break;
-				//		default:
-				//			break;
-				//		}
-
-				//	}
-				//} 
-				//else if (ex.Status == WebExceptionStatus.ConnectFailure) 
-				//{
-				//	//DisplayAlert ("Aviso", "La aplicación no está disponible.", "OK");
-				//}
-			}
-			return null;
-		}
+            return new Requester()
+            {
+                _id = authUser.DetailedUserId._id,
+                PhoneNumber = authUser.DetailedUserId.PhoneNumber,
+                Name = authUser.DetailedUserId.Name,
+                LastName = authUser.DetailedUserId.LastName,
+                GeneralCalification = authUser.DetailedUserId.GeneralCalification
+            };
+        }
     }
 }
